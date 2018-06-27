1a51749b54af274dbb4bdf0121a0a9605993397c
makingthematrix <makingthematrix@gmail.com>
2018-06-27 19:48:55 +0200

    arena tests

---
+++ src/arena/arena1_tests.rs
@@ -0,0 +1,15 @@
+#[cfg(test)]
+mod arena_tests {
+    use arena::arena1::*;
+    use std::collections::HashSet;
+
+    use quickcheck::TestResult;
+    use spectral::prelude::*;
+
+    #[test]
+    fn set_from_and_to_vec() {
+        let world = World::new(100);
+
+        assert_that!(true);
+    }
+}
